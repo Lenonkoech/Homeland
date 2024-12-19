5468af26afa63d73050355642897a7ad4cf8e23a
Lenonkoech <kipyegonlenon226@gmail.com>
2024-12-19 16:36:36 +0300

    Session creation and validation

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 define("APPURL", 'http://localhost/homeland/');
 ?>
 <!DOCTYPE html>
@@ -30,3 +31,70 @@ define("APPURL", 'http://localhost/homeland/');
   <link rel="stylesheet" href="<?php echo APPURL ; ?>css/style.css">
 
 </head>
+<body>
+
+  <div class="site-loader"></div>
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+
+    <div class="site-navbar mt-4">
+      <div class="container py-1">
+        <div class="row align-items-center">
+          <div class="col-8 col-md-8 col-lg-4">
+            <h1 class="mb-0"><a href="index.php" class="text-white h2 mb-0"><strong>Homeland<span
+                    class="text-danger">.</span></strong></a></h1>
+          </div>
+          <div class="col-4 col-md-4 col-lg-8">
+            <nav class="site-navigation text-right text-md-right" role="navigation">
+
+              <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
+                  class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+
+              <ul class="site-menu js-clone-nav d-none d-lg-block">
+                <li class="active">
+                  <a href="<?php echo APPURL;?>index.php">Home</a>
+                </li>
+                <li><a href="<?php echo APPURL;?>buy.php">Buy</a></li>
+                <li><a href="<?php echo APPURL;?>rent.php">Rent</a></li>
+                <li class="has-children">
+                  <a href="<?php echo APPURL;?>properties.php">Properties</a>
+                  <ul class="dropdown arrow-top">
+                    <li><a href="#">Condo</a></li>
+                    <li><a href="#">Property Land</a></li>
+                    <li><a href="#">Commercial Building</a></li>
+
+                  </ul>
+                </li>
+                <li><a href="<?php echo APPURL;?>about.php">About</a></li>
+                <li><a href="<?php echo APPURL;?>contact.php">Contact</a></li>
+
+                 <?php if(isset($_SESSION['username'])) :?> 
+
+                <li class="has-children">
+                  <a href="#"><?php echo $_SESSION['username']?><?php echo"&nbsp&nbsp&nbsp";?></a>
+                  <ul class="dropdown arrow-top">
+                    <li><a href="<?php echo APPURL;?>/auth/logout.php">Logout</a></li> 
+                  </ul>
+                </li>
+              <?php else : ?>
+                <li><a href="<?php echo APPURL;?>auth/login.php">Login</a></li>
+                <li><a href="<?php echo APPURL;?>auth/register.php">Register</a></li>
+              <?php endif ; ?>
+              </ul>
+            </nav>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+ </div>
